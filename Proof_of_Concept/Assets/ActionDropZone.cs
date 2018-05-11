b6de3ecc2cd56dd00f50aa8254ab4848e81cdac2
Matthew Lima <ml396@njit.edu>
2018-05-10 21:16:45 -0400

    OK

---
+++ Proof_of_Concept/Assets/ActionDropZone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class ActionDropZone : UnityEngine.MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Draggable.Slot typeOfCard = Draggable.Slot.INVENTORY;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null)
+        {
+            if (typeOfCard == d.typeOfCard || typeOfCard == Draggable.Slot.INVENTORY)
+            {
+                d.ParentToReturnTo = this.transform;
+            }
+
+        }
+    }
+
+}
---
+++ Proof_of_Concept/Assets/Draggable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : UnityEngine.MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    UnityEngine.Transform parentToReturnTo = null;
+
+    public enum Slot { ACTION, POWER, INVENTORY};
+    public Slot typeOfCard = Slot.ACTION;
+
+    public Transform ParentToReturnTo
+    {
+        get
+        {
+            return parentToReturnTo;
+        }
+
+        set
+        {
+            parentToReturnTo = value;
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+
+        ParentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+
+        GetComponent<UnityEngine.CanvasGroup>().blocksRaycasts = false;
+	}
+	
+	public void OnDrag(PointerEventData eventData) {
+
+        this.transform.position = eventData.position;
+	}
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        this.transform.SetParent(ParentToReturnTo);
+        GetComponent<UnityEngine.CanvasGroup>().blocksRaycasts = true;
+    }
+}
